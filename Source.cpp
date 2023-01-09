db2bbb1987ed0d4a2d78766c8dc425f99ad4c41f
rom341 <roman.murzik.antpnov@gmail.com>
2023-01-09 14:06:17 +0400

    task 08.01.2023 v1.01
    v1.01

--- Source.cpp
+++ Source.cpp
@@ -1,40 +1,55 @@
 ﻿#include<iostream>
 using namespace std;
-const int n = 10;
-struct Attributes
+const int n = 2;
+class Attributes
 {
 	string name;
 	int instance;
 	bool multiInstance;
 	string value;
+public: void SetAttributes()
+{
+	cout << "Enter Atribute name: ";
+	cin >> name;
+
+	cout << "Enter Atribute instance: ";
+	cin >> instance;
+
+	cout << "Enter Atribute multiInstance: ";
+	cin >> multiInstance;
+
+	cout << "Enter Atribute value: ";
+	cin >> value;
+}
 };
-struct Component
+class Component
 {
 	string name;
 	string source;
 	Attributes attribute[n];
+
+public:
+	void SetComponent()
+	{
+		cout << "Enter Component name: ";
+		cin >> name;
+		cout << "Enter Component source: ";
+		cin >> source;
+	}
+	void SetAttributes()
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << "=====Attribute № " << i + 1 << "=====" << endl;
+			attribute[i].SetAttributes();
+		}
+	}
 };
 
 void main()
 {
 	Component comp;
-	cout << "Enter Component name: ";
-	cin >> comp.name;
-	cout << "Enter Component source: ";
-	cin >> comp.source;
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << "Enter Atribute №" << i + 1 << " name: ";
-		cin >> comp.attribute[i].name;
-
-		cout << "Enter Atribute №" << i + 1 << " instance: ";
-		cin >> comp.attribute[i].instance;
-
-		cout << "Enter Atribute №" << i + 1 << " multiInstance: ";
-		cin >> comp.attribute[i].multiInstance;
-
-		cout << "Enter Atribute №" << i + 1 << " value: ";
-		cin >> comp.attribute[i].value;
-	}
+	comp.SetComponent();
+	comp.SetAttributes();
+	cout << endl << "Work finished!" << endl;
 }
